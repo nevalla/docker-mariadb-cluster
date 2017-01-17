0d258c0f8cb51e9b31291e34c8fba1d4b3a6a808
Lauri Nevala <lauri.nevala@gmail.com>
2017-01-17 14:40:04 +0200

    Use simple DNS addresses on cluster address

--- scripts/init_cluster_conf.sh
+++ scripts/init_cluster_conf.sh
@@ -8,14 +8,14 @@ CLUSTER_ADDRESS="gcomm://$CLUSTER_MEMBERS?pc.wait_prim=no"
 # we use dns service discovery to find other members when in service mode
 # and set/override cluster_members provided by ENV
 if [ -n "$DB_SERVICE_NAME" ]; then
-  
+
   # we check, if we have to enable bootstrapping, if we are the only/first node live
-  if [ `getent hosts tasks.$DB_SERVICE_NAME|wc -l` = 1 ] ;then 
+  if [ `getent hosts $DB_SERVICE_NAME|wc -l` = 1 ] ;then
     # bootstrapping gets enabled by empty gcomm string
     CLUSTER_ADDRESS="gcomm://"
   else
     # we fetch IPs of service members
-    CLUSTER_MEMBERS=`getent hosts tasks.$DB_SERVICE_NAME|awk '{print $1}'|tr '\n' ','`
+    CLUSTER_MEMBERS=`getent hosts $DB_SERVICE_NAME|awk '{print $1}'|tr '\n' ','`
     # we set gcomm string with found service members
     CLUSTER_ADDRESS="gcomm://$CLUSTER_MEMBERS?pc.wait_prim=no"
   fi
@@ -26,26 +26,26 @@ fi
 config_file="/etc/mysql/conf.d/galera.cnf"
 
 cat <<EOF > $config_file
-# Node specifics 
-[mysqld] 
+# Node specifics
+[mysqld]
 # next 3 params disabled for the moment, since they are not mandatory and get changed with each new instance.
-# they also triggered problems when trying to persist data with a backup service, since also the config has to be 
+# they also triggered problems when trying to persist data with a backup service, since also the config has to be
 # persisted, but HOSTNAME changes at container startup.
-#wsrep-node-name = $HOSTNAME 
-#wsrep-sst-receive-address = $HOSTNAME
-#wsrep-node-incoming-address = $HOSTNAME
+wsrep-node-name = $HOSTNAME
+wsrep-sst-receive-address = $HOSTNAME
+wsrep-node-incoming-address = $HOSTNAME
 
 # Cluster settings
 wsrep-on=ON
-wsrep-cluster-name = "$CLUSTER_NAME" 
+wsrep-cluster-name = "$CLUSTER_NAME"
 wsrep-cluster-address = $CLUSTER_ADDRESS
-wsrep-provider = /usr/lib/galera/libgalera_smm.so 
-wsrep-provider-options = "gcache.size=256M;gcache.page_size=128M;debug=no" 
-wsrep-sst-auth = "$GALERA_USER:$GALERA_PASS" 
+wsrep-provider = /usr/lib/galera/libgalera_smm.so
+wsrep-provider-options = "gcache.size=256M;gcache.page_size=128M;debug=no"
+wsrep-sst-auth = "$GALERA_USER:$GALERA_PASS"
 wsrep_sst_method = rsync
-binlog-format = row 
-default-storage-engine = InnoDB 
-innodb-doublewrite = 1 
-innodb-autoinc-lock-mode = 2 
-innodb-flush-log-at-trx-commit = 2 
+binlog-format = row
+default-storage-engine = InnoDB
+innodb-doublewrite = 1
+innodb-autoinc-lock-mode = 2
+innodb-flush-log-at-trx-commit = 2
 EOF
